e6febc3c62122f95f4eafceb38c79d93a96411db
wiktorsaw27 <wiktorsawicki01@o2.pl>
2025-11-17 18:12:36 +0100

    Dodane wszystkie produkty

--- src/Products.js
+++ src/Products.js
@@ -1,5 +1,13 @@
-import product1 from './Assets/gramofon.jpg'
-
+import product1 from './Assets/gramofon-test.webp'
+import product2 from './Assets/trombka.jpg'
+import product3 from './Assets/flet.jpg'
+import product4 from './Assets/gitara.jpg'
+import product5 from './Assets/piano.jpg'
+import product6 from './Assets/qq.jpg'
+import product7 from './Assets/perkusja.jpg'
+import product8 from './Assets/organy.jpg'
+import product9 from './Assets/bemben.jpg'
+import product10 from './Assets/skrzypce.jpg'
 
 export const PRODUCTS = [
     {
@@ -12,24 +20,54 @@ export const PRODUCTS = [
         id: 2,
         productName: 'Test1',
         price: 999,
-        productImage: product1,
+        productImage: product2,
     },
     {
         id: 3,
         productName: 'Test2',
         price: 2299,
-        productImage: product1,
+        productImage: product3,
     },
     {
         id: 4,
         productName: 'Test3',
         price: 1299,
-        productImage: product1,
+        productImage: product4,
     },
     {
         id: 5,
         productName: 'Test4',
         price: 799,
-        productImage: product1,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: 'Test5',
+        price: 799,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: 'Test6',
+        price: 799,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: 'Test7',
+        price: 799,
+        productImage: product8,
+    },
+    {
+        id: 9,
+        productName: 'Test8',
+        price: 799,
+        productImage: product9,
+    },
+    {
+        id: 10,
+        productName: 'Test9',
+        price: 799,
+        productImage: product10,
     },
 ];
